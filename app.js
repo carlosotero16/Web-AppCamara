d08c84df3338404c4ba062389f03f24cd3758f76
carlosotero16 <45101852+carlosotero16@users.noreply.github.com>
2018-11-16 12:02:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,29 @@
-
-
 // Set constraints for the video stream
-
-var constraints = { video: { facingMode: "user" }, audio: false };
-var track = null;
-
+var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
-const video = document.querySelector("#video"),
+const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
-
-// Access the device camera and stream to video ////seleccion de camara frontal o trasera
-function start() {
-  if (window.stream) {
-    window.stream.getTracks().forEach(track => {
-      track.stop();
-    });
-  }
-  const audioSource = audioInputSelect.value;
-  const videoSource = videoSelect.value;
-  const constraints = {
-    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-  };
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
-}
-
-audioInputSelect.onchange = start;
-audioOutputSelect.onchange = changeAudioDestination;
-
-videoSelect.onchange = start;
-
-start();
-
-
-
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
-            track = stream.getTracks()[0];
-            video.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops. Algo salió mal.", error);
-        });
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
 }
-
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = video.videoWidth;
-    cameraSensor.height = video.videoHeight;
-    cameraSensor.getContext("2d").drawImage(video, 0, 0);
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-    // track.stop();
 };
-
-function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  video.srcObject = stream;
-  // Refresh button list in case labels have become available
-  return navigator.mediaDevices.enumerateDevices();
-}
-
-
-function start() {
-  if (window.stream) {
-    window.stream.getTracks().forEach(track => {
-      track.stop();
-    });
-  }
-  const audioSource = audioInputSelect.value;
-  const videoSource = videoSelect.value;
-  const constraints = {
-    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
-    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
-  };
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
-}
 // Start the video stream when the window loads
-//window.addEventListener("load", cameraStart, false);
-videoSelect.onchange = start;
-
-// Install ServiceWorker
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register( '/Web-AppCamara/sw.js' , { scope : ' ' } ).then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
-  });
-} else {
-  console.log('CLIENT: service worker is not supported.');
-}
-
+window.addEventListener("load", cameraStart, false);
