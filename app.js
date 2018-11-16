22b2e5dd7fccd6c2dd84888f96c810c2e2ea007d
carlosotero16 <45101852+carlosotero16@users.noreply.github.com>
2018-11-16 12:21:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function cameraStart() {
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.height = "320";
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
