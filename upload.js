238386edbd263771add5d61ef561192436b2722e
carlosotero16 <45101852+carlosotero16@users.noreply.github.com>
2018-11-23 12:50:55 -0600

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,4 +1,5 @@
 
+
 function ObtenerImagenWS() {
 
     const url = "https://kontrata-ocr-api.herokuapp.com/recognize";
@@ -19,6 +20,8 @@ function ObtenerImagenWS() {
 
         fetch(url, {
             method: 'POST',
+            credentials: 'same-origin',
+            mode: 'cors',
             body: formData
         }).then(response =>response.json())
           .then(data => {
@@ -30,43 +33,14 @@ function ObtenerImagenWS() {
 
 
 
-        //const url = "https://kontrata-ocr-api.herokuapp.com/recognize";
-        //const form = document.querySelector('form');
-
-        //form.addEventListener('submit', e => {
-        //    e.preventDefault();
-
-        //    const files = document.querySelector('[type=file]').files;
-        //    const formData = new FormData();
-        //    var resultado = "";
-
-        //    for (let i = 0; i < files.length; i++) {
-        //        let file = files[i];
-
-        //        formData.append('image', file);
-        //    }
-
-        //    fetch(url, {
-        //        method: 'POST',
-        //        body: formData
-        //    }).then(response => response.json())
-        //      .then(data => {
-        //          console.log(data)
-
-        //      });
-
-        //    alert("resultado: " + resultado);
-
-
-        //}).then(response => {
-        //    console.log(response);
-        //});
+       
     });
 
 
 
 
 }
+
 
 
 
