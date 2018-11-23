cc2c5da686f389ed7fa60e8a54f693c5d254f2b3
carlosotero16 <45101852+carlosotero16@users.noreply.github.com>
2018-11-23 08:51:58 -0600

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,72 @@
+
+function ObtenerImagenWS() {
+
+    const url = "https://kontrata-ocr-api.herokuapp.com/recognize";
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const files = document.querySelector('[type=file]').files;
+        const formData = new FormData();
+        var resultado = "";
+
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+
+            formData.append('image', file);
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        }).then(response =>response.json())
+          .then(data => {
+              console.log(data)
+              resultado = JSON.stringify(data)
+
+              alert("Numeracion: " + resultado);
+          });
+
+
+
+        //const url = "https://kontrata-ocr-api.herokuapp.com/recognize";
+        //const form = document.querySelector('form');
+
+        //form.addEventListener('submit', e => {
+        //    e.preventDefault();
+
+        //    const files = document.querySelector('[type=file]').files;
+        //    const formData = new FormData();
+        //    var resultado = "";
+
+        //    for (let i = 0; i < files.length; i++) {
+        //        let file = files[i];
+
+        //        formData.append('image', file);
+        //    }
+
+        //    fetch(url, {
+        //        method: 'POST',
+        //        body: formData
+        //    }).then(response => response.json())
+        //      .then(data => {
+        //          console.log(data)
+
+        //      });
+
+        //    alert("resultado: " + resultado);
+
+
+        //}).then(response => {
+        //    console.log(response);
+        //});
+    });
+
+
+
+
+}
+
+
+
