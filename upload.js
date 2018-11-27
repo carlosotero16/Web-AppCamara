fe2b5aeedbab75166490ee94c85285413370b8d6
carlosotero16 <45101852+carlosotero16@users.noreply.github.com>
2018-11-27 13:18:21 -0600

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,4 @@
 
-
-
 function ObtenerImagenWS() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = "https://kontrata-ocr-api.herokuapp.com/recognize";
@@ -29,7 +27,7 @@ function ObtenerImagenWS() {
               console.log(data)
               resultado = JSON.stringify(data)
               ObtenerTexto(resultado)
-              alert("Numeracion: " + resultado);
+             // alert("Numeracion: " + resultado);
           }).catch(function (error) {
               alert("Error: " + error);
           });
@@ -47,29 +45,69 @@ function ObtenerImagenWS() {
 
 function ObtenerTexto(resultado) {
 
-    var texto = resultado;
-    var variable1 = "";
-    var variable2 = "";
-    var variable3 = "";
-    var variable4 = "";
+    //var texto = resultado;
+    //var variable1 = "";
+    //var variable2 = "";
+    //var variable3 = "";
+    //var variable4 = "";
 
-    //obtiene el cod de acuerdo al resultado del ws:
-    var resCodCuenta = texto.substring(10, 14);
-    var resCodCBanco = texto.substring(15, 25);
-    var resCodRuta = texto.substring(26, 33);
+    ////obtiene el cod de acuerdo al resultado del ws:  {"text":"@184@[00502-010[70-5555@"}
+    //var resCodCuenta = texto.substring(10, 13);
+    //var resCodCBanco = texto.substring(15, 24);
+    //var resCodRuta = texto.substring(26, 32);
 
-    //muestra mensaje con el resultado 
+    ////muestra mensaje con el resultado 
+
+    //alert("Resultado Final: " +
+    //    "\nCodigo Cuenta: " + resCodCuenta +
+    //    "\nCodigo Banco: " + resCodCBanco +
+    //    "\nCodigo Ruta: " + resCodRuta);
+
+
+
+    var test_str = resultado;
+    //codigo cuenta
+    var CodCuentastart_pos = test_str.indexOf('@') + 1;
+    var CodCuentaend_pos = test_str.indexOf('@', CodCuentastart_pos);
+
+    var codCuenta = test_str.substring(CodCuentastart_pos, CodCuentaend_pos)
+
+    //codigo banco
+    var CodBancotart_pos = test_str.indexOf('[') + 1;
+    var CodBancoend_pos = test_str.indexOf('[', CodBancotart_pos);
+
+    var codBanco = test_str.substring(CodBancotart_pos, CodBancoend_pos)
+
+
+    //transito
+    var CodTransitotart_pos = test_str.indexOf('[') + 1;
+    var CodTransitoend_pos = test_str.indexOf('-', CodTransitotart_pos);
+
+    var codTransito = test_str.substring(CodTransitotart_pos, CodTransitoend_pos)
+
+
+
+
+
+    //codigo ruta
+
+   // var eliminarC = resultado.replace(/[[]/, "-");
+
+    var CodRutatart_pos = test_str.indexOf('@[') + 1;
+    var CodRutaend_pos = test_str.indexOf('@"', CodRutatart_pos);
+
+    var codRuta = test_str.substring(CodRutatart_pos, CodRutaend_pos)
+
+   
 
     alert("Resultado Final: " +
-        "\nCodigo Cuenta: " + resCodCuenta +
-        "\nCodigo Banco: " + resCodCBanco +
-        "\nCodigo Ruta: " + resCodRuta);
+     "\nCodigo Cuenta: " + codCuenta +
+     "\nCodigo Banco: " + codBanco +
+     "\nCodigo Transito: "+ codTransito+
+     "\nCodigo Ruta: " + codRuta.replace(/[[]/g, " "));
+
 
 
 }
-
-
-
-
 
 
